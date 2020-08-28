95c726ac00e031436677a06327644decbcc1bb3c
AnushaV1 <anush_chander@yahoo.com>
2020-08-27 22:43:33 -0700

    tests modified

--- itemRoutes.test.js
+++ itemRoutes.test.js
@@ -16,59 +16,59 @@ afterEach(function () {
 });
 
 describe("GET /items", () => {
-  test("Get all items", async () => {
+test("Get all items", async () => {
     const res = await request(app).get("/items");
     expect(res.statusCode).toBe(200)
     expect(res.body).toEqual({ items: [{name:"popsicle", price: 1.45}] })
-  })
+})
 })
 
 describe("GET /items/:name", () => {
-  test("Get items by name", async () => {
+test("Get items by name", async () => {
     const res = await request(app).get(`/items/${item.name}`);
     expect(res.statusCode).toBe(200)
     expect(res.body).toEqual({ item: {name:"popsicle", price: 1.45} })
-  })
-  test("Responds with 404 for invalid item", async () => {
+})
+test("Responds with 404 for invalid item", async () => {
     const res = await request(app).get(`/items/icecube`);
     expect(res.statusCode).toBe(404)
-  })
+})
 })
 
 describe("POST /items", () => {
-  test("Creating a item", async () => {
+test("Creating a item", async () => {
     const res = await request(app).post("/items").send({ name: "snickers", price: 1.25 });
     expect(res.statusCode).toBe(201);
     expect(res.body).toEqual({ added: { name: "snickers", price: 1.25 } });
-  })
+})
 
-  test("Responds with 400 if name is missing", async () => {
+test("Responds with 400 if name is missing", async () => {
     const res = await request(app).post("/items").send({});
     expect(res.statusCode).toBe(400);
-  })
- })
+})
+})
 
 describe("/PATCH /items/:name", () => {
-  test("Updating a item's name", async () => {
+test("Updating a item's name", async () => {
     const res = await request(app).patch(`/items/${item.name}`).send({ name: "Monster", price:0.99 });
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({ item: { name: "Monster", price:0.99 } });
-  })
-  test("Responds with 404 for invalid name", async () => {
+})
+test("Responds with 404 for invalid name", async () => {
     const res = await request(app).patch(`/items/skittles`).send({ name: "Monster" });
     expect(res.statusCode).toBe(404);
-  })
+})
 })
 
 describe("/DELETE /items/:name", () => {
-  test("Deleting an item", async () => {
+test("Deleting an item", async () => {
     const res = await request(app).delete(`/items/${item.name}`);
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({ message: 'Deleted' })
-  })
-  test("Responds with 404 for deleting invalid item", async () => {
+})
+test("Responds with 404 for deleting invalid item", async () => {
     const res = await request(app).delete(`/items/burger`);
     expect(res.statusCode).toBe(404);
-  })
+})
 })
 
